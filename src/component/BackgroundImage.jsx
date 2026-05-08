7da9f2bf908a6b6d3b4cb8007d07700d18b1b145
Mouhssine Es-Safy <myschoolccfbs@gmail.com>
2026-05-08 16:40:05 +0100

    I changed the background image and some icons

--- src/component/BackgroundImage.jsx
+++ src/component/BackgroundImage.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./BackgroundImage.css";
+import "../assets/icon/iconTX.png";
 
 const BackgroundImage = () => {
   return (
     <section className="hero">
       <div className="content">
-        <h1>Be Creative.</h1>
+        <h1>
+          Be Creative.
+          <span>
+            {""}
+            <img src="./assets/icon/iconTX.png" alt="" />
+          </span>
+        </h1>
         <p>
           The Mawazine Festival is an annual international music festival
           founded in 2001. It takes place in the Moroccan capital, Rabat,
           between May and June, under the patronage of King Mohammed VI.
         </p>
-        <a href="#">Start</a>
+        <a className="" href="#">
+          Start
+        </a>
       </div>
     </section>
   );
